5e96fa23c4c748fd4ebf428e3a68405421a70a27
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-11-03 13:52:52 -0400

    change to CPU()

--- src/simulations.jl
+++ src/simulations.jl
@@ -24,7 +24,7 @@ end
 
 function word_to_simulation(::Val{:buoyancy_driven}, word;
                             pad_to_square = true,
-                            architecture = GPU(),
+                            architecture = CPU(),
                             extent = (1, 1),
                             halo = (5, 5),
                             topology = (Periodic, Flat, Bounded),
